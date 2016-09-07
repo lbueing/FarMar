ae802668c2552fc4ceaa4253eb3310f244ac75a2
Leah Bueing <lbueing@gmail.com>
2016-09-07 13:08:51 -0700

    tests for all and find for market and vendor

--- specs/FarMar_spec.rb
+++ specs/FarMar_spec.rb
@@ -1,2 +1,23 @@
 require_relative 'Spec_helper'
-require_relative '../lib/FarMar'
+require_relative '../far_mar'
+
+
+describe FarMar do
+
+  it 'Check @@market_info length in Market' do
+    expect( FarMar::Market.all.length ).must_equal(500)
+  end
+
+  it 'Check for proper returnd id info market' do
+    expect( FarMar::Market.find(342).id ).must_equal("342")
+  end
+
+  it 'Check @@market_info length in Vendor' do
+    expect( FarMar::Vendor.all.length ).must_equal(2690)
+  end
+
+  it 'Check for proper returnd id info vendor' do
+    expect( FarMar::Vendor.find(663).id ).must_equal("663")
+  end
+
+end
