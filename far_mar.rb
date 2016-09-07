babd3b2f7cf8cb2333eb58322e5e2072b332d8c7
Leah Bueing <lbueing@gmail.com>
2016-09-07 13:49:22 -0700

    removed csv importing because too much time, added parameters for sale and product

--- far_mar.rb
+++ far_mar.rb
@@ -8,7 +8,7 @@ module FarMar
 
   class AllInfo
 
-  attr_accessor :id, :name, :address, :city, :county, :state, :zip, :num_employees, :market_id, :amount
+  attr_accessor :id, :name, :address, :city, :county, :state, :zip, :num_employees, :market_id, :amount, :purchase_time, :vendor_id, :product_id, :vendor_id
     def initialize(hash_parameter)
       @id = hash_parameter[:id] #fixnum
       @name = hash_parameter[:name] #string
@@ -20,18 +20,26 @@ module FarMar
       @num_employees = hash_parameter[:num_employees] #fixnum
       @market_id = hash_parameter[:maket_id] #fixnum
       @amount = hash_parameter[:amount]
+      @purchase_time = hash_parameter[:purchase_time] #date time
+      @vendor_id = hash_parameter[:vendor_id] #fixnum
+      @product_id = hash_parameter[:product_id] #fixnum
+      @vendor_id = hash_parameter[:vendor_id] #fixnum
     end
 
-    def self.all
-      @market_info = []
-      CSV.open("support/markets.csv", "r").each do |line|
-          @market_info << FarMar::Market.new(id: line[0], name: line[1], address: line[2], city: line[3], country: line[4], state: line[5], zip: line[6])
-        end
-        @vendor_info = []
-      CSV.open("support/vendors.csv", "r").each do |line|
-          @vendor_info << FarMar::Vendor.new(id: line[0], name: line[1], num_employees: line[2], market_id: line[3])
-        end
-    end
+    # def self.all
+    #   @market_info = []
+    #   CSV.open("support/markets.csv", "r").each do |line|
+    #       @market_info << FarMar::Market.new(id: line[0], name: line[1], address: line[2], city: line[3], country: line[4], state: line[5], zip: line[6])
+    #     end
+      #   @vendor_info = []
+      # CSV.open("support/vendors.csv", "r").each do |line|
+      #     @vendor_info << FarMar::Vendor.new(id: line[0], name: line[1], num_employees: line[2], market_id: line[3])
+      #   end
+      #   @sale_info = []
+      # CSV.open("support/sales.csv", "r").each do |line|
+      #     @sale_info << FarMar::Sale.new(id: line[0], amount: line[1], purchase_time: line[2], vendor_id: line[3], product_id: line[4])
+      #   end
+    # end
   end
 
 
