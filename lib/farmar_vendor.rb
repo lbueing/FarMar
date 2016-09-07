01161deecbc067b5cc74b8549513cc9f3f2764cd
Leah Bueing <lbueing@gmail.com>
2016-09-07 13:07:10 -0700

    working self.all and self.find

--- lib/farmar_vendor.rb
+++ lib/farmar_vendor.rb
@@ -1,4 +1,23 @@
 # lib/farmar_market.rb
-class FarMar::Vendor
-  # Your code goes here
+class FarMar::Vendor < FarMar::AllInfo
+
+  attr_accessor :id, :name, :num_employees, :market_id
+  def initialize(hash_parameter)
+    super
+  end
+
+  def self.all
+    super
+    return @vendor_info
+  end
+
+  def self.find(id)
+    all
+    @vendor_info.each do |i|
+      if i.id.to_i == id
+        return i
+      end
+    end
+  end
+
 end
